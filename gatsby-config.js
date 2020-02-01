b0f3dcce1969bfdc083ecdaa73a3891aa787936a
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-02-01 13:37:50 +0100

    nicer favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,15 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-                name: 'gatsby-starter-default',
-                short_name: 'starter',
+                name: 'Alchymistův učeň',
+                short_name: 'Alchymistův učeň',
                 start_url: '/',
                 background_color: '#663399',
                 theme_color: '#663399',
                 display: 'minimal-ui',
-                icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+                icon: 'src/images/squared-circle.svg',
             },
         },
         'gatsby-plugin-sass',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     ],
 };
