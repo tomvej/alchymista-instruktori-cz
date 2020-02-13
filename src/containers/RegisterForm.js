9a8fe7c170b809eb21a220cd48a2ae3e677afb99
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-02-13 16:12:03 +0100

    register text

--- src/containers/RegisterForm.js
+++ src/containers/RegisterForm.js
@@ -7,7 +7,8 @@ import {required, validEmail, renderMarkdown} from '../utils';
 export default () => {
     const {
         site: {siteMetadata: {formFields: {name, email, message, photoConsent}}},
-        consentText: {childMarkdownRemark: {htmlAst}},
+        consentText: {childMarkdownRemark: {htmlAst: consentAst}},
+        registerText: {childMarkdownRemark: {htmlAst: registerAst}}
     } = useStaticQuery(graphql`
         query {
             site {
@@ -25,52 +26,60 @@ export default () => {
                     htmlAst
                 }
             }
+            registerText: file(relativePath: {eq: "register.md"}) {
+                childMarkdownRemark {
+                    htmlAst
+                }
+            }
         }
     `);
 
     return (
-        <FinalForm
-            onSubmit={(values) => console.log(values)}
-            initialValues={{[photoConsent]: true}}
-        >
-            {({handleSubmit, valid}) => (
-                <Form onSubmit={handleSubmit}>
-                    <SideBySide
-                        left={(
-                            <FormField
-                                label="Jméno a příjmení"
-                                placeholder="Tvoje jméno"
-                                name={name}
-                                validate={[required]}
-                                component={StringInput}
-                            />
-                        )}
-                        right={(
-                            <FormField
-                                label="E-mail"
-                                placeholder="Tvůj e-mail"
-                                name={email}
-                                validate={[required, validEmail]}
-                                component={StringInput}
-                            />
-                        )}
-                    />
-                    <FormField
-                        name={message}
-                        placeholder="Chceš nám něco vzkázat?"
-                        component={StringInput}
-                        area
-                    />
-                    <CheckboxField
-                        name={photoConsent}
-                        label="Uděluji souhlas s focením"
-                    />
-                    <ConsentArea>
-                        {renderMarkdown(htmlAst)}
-                    </ConsentArea>
-                    <Button submit disabled={!valid}>Přihlásit se</Button>
-                </Form>
-            )}
-        </FinalForm>
+        <>
+            {renderMarkdown(registerAst)}
+            <FinalForm
+                onSubmit={(values) => console.log(values)}
+                initialValues={{[photoConsent]: true}}
+            >
+                {({handleSubmit, valid}) => (
+                    <Form onSubmit={handleSubmit}>
+                        <SideBySide
+                            left={(
+                                <FormField
+                                    label="Jméno a příjmení"
+                                    placeholder="Tvoje jméno"
+                                    name={name}
+                                    validate={[required]}
+                                    component={StringInput}
+                                />
+                            )}
+                            right={(
+                                <FormField
+                                    label="E-mail"
+                                    placeholder="Tvůj e-mail"
+                                    name={email}
+                                    validate={[required, validEmail]}
+                                    component={StringInput}
+                                />
+                            )}
+                        />
+                        <FormField
+                            name={message}
+                            placeholder="Chceš nám něco vzkázat?"
+                            component={StringInput}
+                            area
+                        />
+                        <CheckboxField
+                            name={photoConsent}
+                            label="Uděluji souhlas s focením"
+                        />
+                        <ConsentArea>
+                            {renderMarkdown(consentAst)}
+                        </ConsentArea>
+                        <Button submit disabled={!valid}>Přihlásit se</Button>
+                    </Form>
+                )}
+            </FinalForm>
+        </>
     );
 };
---
+++ src/text/register.md
@@ -0,0 +1,6 @@
+1. Vyplň a odešli formulář dole.
+2. Přijde ti dotazník, ve kterém se tě zeptáme … na co se zeptáme?
+3. Až od tebe dostaneme vyplněný dotazník, rozhodneme se, jestli tě na kurz vezmeme. Dáme ti vědět do 14 dnů od obdržení dotazníku.
+4. Pošleme ti platební údaje. Teprve po tom, co nám zaplatíš, s tebou počítáme.
+
+Může se stát, že tě na základě dotazníku na kurz nevezmeme, například pro to, že od něj očekáváš něco, co ti nemůže dát. V takovém případě tě rádi uvidíme na jiných akcích Instruktorů Brno.
