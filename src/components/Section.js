be24d12d996b4dec93e2cfb2c106487b6325bda0
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-03-20 14:47:20 +0100

    margin collapse fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import {Element} from 'react-scroll';
 
 import ResponsiveContainer from './ResponsiveContainer';
+import style from './Section.module.scss';
 
 const Section = ({name, children, narrow}) => (
     <section id={name}>
         <ResponsiveContainer narrow={narrow}>
-            <Element name={name}>
+            <Element name={name} className={style.main}>
                 {children}
             </Element>
         </ResponsiveContainer>
