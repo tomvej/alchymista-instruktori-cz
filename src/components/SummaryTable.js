530c2a38f8d565e861a38e78eba27a35c31d5dcb
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-03-26 15:18:51 +0100

    sumary table reimagined

--- src/components/SummaryTable.js
+++ src/components/SummaryTable.js
@@ -8,17 +8,22 @@ import ScrollLink from './ScrollLink';
 import style from './SummaryTable.module.scss';
 
 const SummaryTable = ({items, actionLink, actionText}) => (
-    <dl className={style.main}>
-        {items.map(({label, value}) => (
-            <div className={style.itemWrapper} key={label}>
-                <div className={style.item}>
-                    <dt className={style.label}>{label}</dt>
-                    <dd className={style.content}>{value}</dd>
+    <div className={style.main}>
+        <dl className={style.list}>
+            {items.map(({label, value}) => (
+                <div key={label} className={style.item}>
+                    <div className={style.itemList}>
+                        <dt className={style.label}>{label}</dt>
+                        <dd className={style.content}>{value}</dd>
+                    </div>
                 </div>
-            </div>
-        ))}
-        <ScrollLink to={actionLink} className={style.action}>{actionText} <FAIcon icon={faArrowRight} className={style.icon} /></ScrollLink>
-    </dl>
+            ))}
+        </dl>
+        <ScrollLink to={actionLink} className={style.action}>
+            {actionText}
+            <FAIcon icon={faArrowRight} className={style.icon} />
+        </ScrollLink>
+    </div>
 );
 
 SummaryTable.propTypes = {
