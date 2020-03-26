69b813114cce6ce3b5bc3c25ce58a2fac187b8cf
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-03-26 21:16:32 +0100

    about section styling

--- src/components/index.js
+++ src/components/index.js
@@ -27,3 +27,4 @@ export {default as TwoColumnGallery} from './TwoColumnGallery';
 export {default as MultiRowGallery} from './MultiRowGallery';
 export {createMarkdownRenderer, renderMarkdown} from './markdown';
 export {default as Unbreakable} from './Unbreakable';
+export {default as AboutSection} from './AboutSection';
