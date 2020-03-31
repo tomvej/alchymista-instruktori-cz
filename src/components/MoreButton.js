425090e08c860f25894861fee80d63e6acf7eb21
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-03-31 10:23:15 +0200

    more button styling

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome';
+import {faChevronCircleDown, faChevronDown} from '@fortawesome/free-solid-svg-icons';
 import ScrollLink from './ScrollLink';
 
+import style from './MoreButton.module.scss';
+
 const MoreButton = ({to}) => (
-    <ScrollLink to={to}>More</ScrollLink>
+    <ScrollLink to={to} className={classnames(style.main, 'fa-layers')} aria-label="Více">
+        <FAIcon icon={faChevronDown} className={style.icon} />
+        <FAIcon icon={faChevronCircleDown} className={style.background} />
+    </ScrollLink>
 );
 
 MoreButton.propTypes = {
